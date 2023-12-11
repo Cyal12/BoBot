8b969f06f13d89fcf0870c1fb80275c30d593de8
Carles <79943165+Cyal12@users.noreply.github.com>
2023-12-11 15:24:15 -0600

    Optimizing code

--- public/js/script.js
+++ public/js/script.js
@@ -1,42 +1,64 @@
 let isWelcomeMessageShown = false;
 
-document.getElementById('chat-button').addEventListener('click', function() {
+document.getElementById('chat-button').addEventListener('click', toggleChat);
+document.getElementById('minimize-button').addEventListener('click', minimizeChat);
+document.getElementById('fullscreen-button').addEventListener('click', toggleFullscreen);
+document.getElementById('user-input').addEventListener('keypress', handleUserInput);
+document.getElementById('enter').addEventListener('click', () => handleUserInput({ key: 'Enter' }));
+
+function toggleChat() {
     const chatContainer = document.getElementById('chat-container');
     chatContainer.classList.toggle('expanded');
+    document.getElementById('chat-button').style.display = chatContainer.classList.contains('expanded') ? 'none' : 'block';
+
     if (!isWelcomeMessageShown) {
-        const chatBox = document.getElementById('chat-box');
-        const welcomeMessage = createMessageElement('Welcome to BoBot :D ! How can I help you today?', 'bot-message');
-        chatBox.appendChild(welcomeMessage);
+        appendMessage('Welcome to BoBot :D ! How can I help you today?', 'bot-message');
         isWelcomeMessageShown = true;
     }
+}
 
-    const chatButton = document.getElementById('chat-button');
-    chatButton.style.display = chatContainer.classList.contains('expanded') ? 'none' : 'block';
-});
-
-document.getElementById('minimize-button').addEventListener('click', function(e) {
+function minimizeChat(e) {
     const chatContainer = document.getElementById('chat-container');
-    const chatButton = document.getElementById('chat-button');
-
     if (chatContainer.classList.contains('expanded')) {
         chatContainer.classList.remove('expanded');
-        chatButton.style.display = 'block';
+        document.getElementById('chat-button').style.display = 'block';
     }
     e.stopPropagation();
-});
+}
 
-document.getElementById('fullscreen-button').addEventListener('click', function() {
+function toggleFullscreen() {
     const chatContainer = document.getElementById('chat-container');
-    if (chatContainer.classList.contains('fullscreen')) {
-        chatContainer.classList.remove('fullscreen');
-    } else {
-        chatContainer.classList.add('fullscreen');
-    }
-});
+    chatContainer.classList.toggle('fullscreen');
+}
 
-function scrollToBottom() {
+function handleUserInput(e) {
+    if (e.key === 'Enter') {
+        const userInput = document.getElementById('user-input');
+        let userMessage = userInput.value.trim();
+        if (userMessage) {
+            appendMessage(userMessage, 'user-message');
+            sendUserMessage(userMessage);
+        }
+        userInput.value = '';
+    }
+}
+
+function appendMessage(message, className, url = null) {
     const chatBox = document.getElementById('chat-box');
-    chatBox.scrollTop = chatBox.scrollHeight; // Scroll to the bottom of the chat box
+    const messageElement = createMessageElement(message, className, url);
+    chatBox.appendChild(messageElement);
+    scrollToBottom();
+}
+
+function disableResponseButtons() {
+    const buttons = document.querySelectorAll('.response-button');
+    buttons.forEach(button => {
+        button.disabled = true;
+    });
+}
+function handleButtonResponse(optionLabel) {
+    sendUserMessage(optionLabel);
+    disableResponseButtons();
 }
 
 function createMessageElement(message, className, url = null) {
@@ -49,172 +71,71 @@ function createMessageElement(message, className, url = null) {
         botImg.alt = "Bot";
         botImg.classList.add('bot-img');
         messageDiv.appendChild(botImg);
+
         const messageText = document.createElement('span');
         messageText.innerHTML = message;
         messageDiv.appendChild(messageText);
-            if (url) {
-                const linkElement = createLinkElement(url, 'bot-message-link');
-                messageDiv.appendChild(linkElement);
-            }
-        } else {
-            messageDiv.textContent = message;
+
+        if (url) {
+            const linkElement = document.createElement('a');
+            linkElement.href = url;
+            linkElement.textContent = "More information";
+            linkElement.className = 'bot-message-link';
+            linkElement.target = "_blank";
+            linkElement.rel = "noopener noreferrer";
+            messageDiv.appendChild(linkElement);
         }
-
-        return messageDiv;
+    } else {
+        messageDiv.textContent = message;
     }
+    return messageDiv;
+}
 
-    function createLinkElement(url, classNames) {
-        const linkElement = document.createElement('a');
-        linkElement.href = url;
-        linkElement.textContent = "More information";
-        linkElement.className = classNames;
-        linkElement.target = "_blank";
-        linkElement.rel = "noopener noreferrer";
-        return linkElement;
-    }
+function scrollToBottom() {
+    const chatBox = document.getElementById('chat-box');
+    chatBox.scrollTop = chatBox.scrollHeight;
+}
 
-    document.getElementById('user-input').addEventListener('keypress', function(event) {
-        if (event.key === 'Enter') {
-            let userMessage = this.value.trim();
-            if (userMessage === '') return; //
+function sendUserMessage(message) {
+    fetch('/user-message', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ message })
+    })
+        .then(response => response.json())
+        .then(data => processAssistantResponse(data))
+        .catch(error => console.error('Error:', error));
+}
 
-            this.value = '';
-            const chatBox = document.getElementById('chat-box');
-            const userMessageElement = createMessageElement(userMessage, 'user-message');
-            chatBox.appendChild(userMessageElement);
-            fetch('/user-message', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({message: userMessage}),
-            })
-                .then(response => response.json())
-                .then(data => {
-                    const chatBox = document.getElementById('chat-box');
-                    if (data.assistantResponse) {
-                        data.assistantResponse.forEach(element => {
-                            if (element.response_type === 'text') {
-                                const textResponse = createMessageElement(marked.parse(element.text), 'bot-message');
-                                chatBox.appendChild(textResponse);
-                            } else if (element.response_type === 'search' && element.primary_results) {
-                                element.primary_results.forEach(result => {
-                                    if (result.answers) {
-                                        result.answers.forEach(answer => {
-                                            const answerElement = createMessageElement(answer.text, 'bot-message', result.url);
-                                            chatBox.appendChild(answerElement);
-                                        });
-                                    }
-                                });
-                            } else if (element.response_type === 'option') {
-                                const buttonsDiv = document.createElement('div');
-                                buttonsDiv.classList.add('bot-message', 'button');
-                                element.options.forEach(option => {
-                                    const button = document.createElement('button');
-                                    button.textContent = option.label;
-                                    button.classList.add('response-button');
-                                    button.addEventListener('click', function() {
-                                        sendResponse(option.label);
-                                    });
-                                    buttonsDiv.appendChild(button);
-                                });
-                                chatBox.appendChild(buttonsDiv);
-                            }
-                            scrollToBottom();
+function processAssistantResponse(data) {
+    const chatBox = document.getElementById('chat-box');
+    if (data.assistantResponse) {
+        data.assistantResponse.forEach(element => {
+            if (element.response_type === 'text') {
+                const textResponse = createMessageElement(marked.parse(element.text), 'bot-message');
+                chatBox.appendChild(textResponse);
+            } else if (element.response_type === 'search' && element.primary_results) {
+                element.primary_results.forEach(result => {
+                    if (result.answers) {
+                        result.answers.forEach(answer => {
+                            const answerElement = createMessageElement(answer.text, 'bot-message', result.url);
+                            chatBox.appendChild(answerElement);
                         });
                     }
-                    chatBox.scrollTop = chatBox.scrollHeight;
-                    chatBox.style.scrollBehavior = 'smooth';
                 });
-        }
-    });
-
-    function disableResponseButtons() {
-        const buttons = document.querySelectorAll('.response-button');
-        buttons.forEach(button => {
-            button.disabled = true;
+            } else if (element.response_type === 'option') {
+                const buttonsDiv = document.createElement('div');
+                buttonsDiv.classList.add('bot-message', 'button');
+                element.options.forEach(option => {
+                    const button = document.createElement('button');
+                    button.textContent = option.label;
+                    button.classList.add('response-button');
+                    button.addEventListener('click', () => handleButtonResponse(option.label));
+                    buttonsDiv.appendChild(button);
+                });
+                chatBox.appendChild(buttonsDiv);
+            }
+            scrollToBottom();
         });
     }
-
-
-    function sendResponse(responseText) {
-        disableResponseButtons();
-        const url = 'http://localhost:3000/user-message';
-        const data = {
-            message: responseText
-        };
-        const fetchOptions = {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(data)
-        };
-        fetch(url, fetchOptions)
-            .then(response => response.json())
-            .then(data => {
-                console.log('Respuesta del servidor:', data);
-                updateChat(data);
-                scrollToBottom()
-            })
-            .catch(error => {
-                console.error('Error al enviar la respuesta:', error);
-            });
-    }
-
-    function enableButtonClickListener() {
-        const buttons = document.querySelectorAll('.response-button');
-        buttons.forEach(button => {
-            button.disabled = false; // Habilitar el botón
-            button.addEventListener('click', function() {
-                const responseText = this.textContent;
-                sendResponse(responseText);
-            });
-        });
-    }
-
-    enableButtonClickListener();
-
-    function updateChat(data) {
-        const chatBox = document.getElementById('chat-box');
-        if (data.assistantResponse) {
-            data.assistantResponse.forEach(element => {
-                if (element.response_type === 'text') {
-                    const textResponse = createMessageElement(marked.parse(element.text), 'bot-message');
-                    chatBox.appendChild(textResponse);
-                } else if (element.response_type === 'search' && element.primary_results) {
-                    element.primary_results.forEach(result => {
-                        if (result.answers) {
-                            result.answers.forEach(answer => {
-                                const messageContainer = document.createElement('div');
-                                messageContainer.className = 'bot-message';
-                                const answerElement = createMessageElement(answer.text, '');
-                                messageContainer.appendChild(answerElement);
-                                if (result.url) {
-                                    const linkElement = createLinkElement(result.url, 'bot-message-link');
-                                    messageContainer.appendChild(linkElement);
-                                }
-                                chatBox.appendChild(messageContainer);
-                            });
-                        }
-                    });
-                } else if (element.response_type === 'option') {
-                    const buttonsDiv = document.createElement('div');
-                    buttonsDiv.classList.add('bot-message', 'button');
-                    element.options.forEach(option => {
-                        const button = document.createElement('button');
-                        button.textContent = option.label;
-                        button.classList.add('response-button');
-                        button.addEventListener('click', function() {
-                            sendResponse(option.label);
-                        });
-                        buttonsDiv.appendChild(button);
-                    });
-                    chatBox.appendChild(buttonsDiv);
-                }
-                scrollToBottom();
-            });
-            chatBox.scrollTop = chatBox.scrollHeight;
-        }
-        chatBox.style.scrollBehavior = 'smooth';
-    }
+}
